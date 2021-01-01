794b8992e45f6b5a42e9f8d43fe88a7ec8296580
Vitaliy Koren <vitaly@simporter.com>
2021-01-01 21:31:27 +0200

    refactored

--- learner.py
+++ learner.py
@@ -10,37 +10,34 @@ class Learner:
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.model = model.to(device)
-        self.metric = metric
-        self.loss_function = loss_function
-        self.optimizer = optimizer
-        self.scheduler = scheduler
-        self.device = device
-        self.grad_accumulation_step = grad_accumulation_step
-        self.logger = logger
+        self._model = model.to(device)
+        self._metric = metric
+        self._loss_function = loss_function
+        self._optimizer = optimizer
+        self._scheduler = scheduler
+        self._device = device
+        self._grad_accumulation_step = grad_accumulation_step
+        self._logger = logger
 
         self.kwargs = kwargs
 
     def fit(self, num_epochs, dataloaders_dict):
 
-        phases = ["train", "valid"]
+        phases = "train", "valid"
 
         for epoch in range(1, num_epochs + 1):
 
             for phase in phases:
 
                 loader = dataloaders_dict[phase]
-                epoch_dict = self.epoch(phase=phase,
-                                        dataloader=loader,
-                                        current_epoch=epoch,
-                                        num_epochs=num_epochs)
+                epoch_dict = self.epoch(phase=phase, dataloader=loader)
 
-                self.logger.add_scalar("{} Epoch Loss".format(phase.title()), epoch_dict["epoch_loss"], epoch)
-                self.logger.add_scalar("{} Epoch Metric".format(phase.title()), epoch_dict["epoch_metric"], epoch)
+                self._logger.add_scalar("{} Epoch Loss".format(phase.title()), epoch_dict["epoch_loss"], epoch)
+                self._logger.add_scalar("{} Epoch Metric".format(phase.title()), epoch_dict["epoch_metric"], epoch)
 
-    def epoch(self, phase, dataloader, current_epoch, num_epochs):
+    def epoch(self, phase, dataloader):
 
-        self.model.train() if phase == "train" else self.model.eval()
+        self._model.train() if phase == "train" else self._model.eval()
 
         running_loss = 0.0
         num_examples = 0
@@ -48,96 +45,67 @@ class Learner:
         epoch_logits = []
         epoch_labels = []
 
-        with tqdm(dataloader) as progress:
+        for step, batch in enumerate(dataloader):
 
-            for step, batch in enumerate(dataloader):
+            batch = self._batch_to_device(batch)
+            labels = batch[-1]
 
-                batch = self._batch_to_device(batch)
-                labels = batch[-1]
+            with torch.set_grad_enabled(phase == "train"):
 
-                with torch.set_grad_enabled(phase == "train"):
+                outputs = self._forward_step(batch)
+                loss = self._compute_loss(outputs=outputs, labels=labels)
 
-                    outputs = self._forward_step(batch)
-                    loss = self._compute_loss(outputs=outputs, labels=labels)
+                loss /= self._grad_accumulation_step
 
-                    loss /= self.grad_accumulation_step
+                if phase == "train":
+                    # loss.backward()
+                    loss.backward()
 
-                    if phase == "train":
-                        # loss.backward()
-                        loss.backward()
+                    # clip_grad_norm
+                    if self.kwargs.get("clip_grad_norm") is not None:
+                        clip_grad_norm_(self._model.parameters(), self.kwargs["clip_grad_norm"])
 
-                        # clip_grad_norm
-                        if self.kwargs.get("clip_grad_norm") is not None:
-                            clip_grad_norm_(self.model.parameters(), self.kwargs["clip_grad_norm"])
+                    # optimizer.step()
+                    if (step + 1) % self._grad_accumulation_step == 0:
+                        self._optimizer.step()
+                        if self._scheduler is not None:
+                            self._scheduler.step()
+                        self._optimizer.zero_grad()
+                        self._log_layer_grads(step=step)
 
-                        # optimizer.step()
-                        if (step + 1) % self.grad_accumulation_step == 0:
-                            self.optimizer.step()
-                            if self.scheduler is not None:
-                                self.scheduler.step()
-                            self.optimizer.zero_grad()
-                            self._log_layer_grads(step=step)
+            # collecting raw outputs to find top losses examples
+            epoch_logits.extend(outputs.detach().cpu().tolist())
+            epoch_labels.extend(labels.detach().cpu().tolist())
 
-                # collecting raw outputs to find top losses examples
-                epoch_logits.extend(outputs.detach().cpu().tolist())
-                epoch_labels.extend(labels.detach().cpu().tolist())
+            # statistics
+            running_loss += loss.item() * labels.size(0)
+            num_examples += labels.size(0)
 
-                # statistics
-                running_loss += loss.item() * labels.size(0)
-                num_examples += labels.size(0)
+        epoch_loss = running_loss / num_examples
+        epoch_metric = self._metric(epoch_labels, epoch_logits)
 
-                self._print_batch_statistics(bar=progress,
-                                             current_epoch=current_epoch,
-                                             loss=loss,
-                                             num_epochs=num_epochs)
-
-            epoch_loss = running_loss / num_examples
-            epoch_metric = self.metric(epoch_labels, epoch_logits)
-
-            epoch_dict = {
-                "epoch_loss": epoch_loss,
-                "epoch_metric": epoch_metric,
-                "epoch_logits": epoch_logits,
-                "epoch_labels": epoch_labels
-            }
-
-            self._print_epoch_statistics(bar=progress,
-                                         epoch=current_epoch,
-                                         phase=phase,
-                                         epoch_dict=epoch_dict,
-                                         num_epochs=num_epochs)
+        epoch_dict = {
+            "epoch_loss": epoch_loss,
+            "epoch_metric": epoch_metric,
+            "epoch_logits": epoch_logits,
+            "epoch_labels": epoch_labels
+        }
 
         return epoch_dict
 
     def _batch_to_device(self, batch):
-        return [tensor.to(self.device) for tensor in batch]
+        return [tensor.to(self._device) for tensor in batch]
 
     def _forward_step(self, batch):
         input_ids = batch[0]
         attention_mask = batch[1]
-        return self.model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        return self._model(input_ids=input_ids, attention_mask=attention_mask)[0]
 
     def _compute_loss(self, outputs, labels):
-        loss = self.loss_function(outputs, labels.long())
+        loss = self._loss_function(outputs, labels.long())
         return loss
 
     def _log_layer_grads(self, step):
-        for name, weight in self.model.named_parameters():
+        for name, weight in self._model.named_parameters():
             if weight.grad is not None:
-                self.logger.add_histogram(f"{name}.grad", weight.grad, step)
-
-    def _print_batch_statistics(self, bar, current_epoch, loss, num_epochs):
-        stats = "Epoch [{}/{}], Loss: {:.4f}".format(
-            current_epoch, num_epochs, loss.item() * self.grad_accumulation_step
-        )
-        bar.set_description(stats)
-        bar.update()
-
-    @staticmethod
-    def _print_epoch_statistics(bar, epoch, phase, epoch_dict, num_epochs):
-        stats = "Epoch [{}/{}],  {} Loss:  {:.4f}  {} Metric:  {:.4f}  |".format(
-            epoch, num_epochs,
-            phase.title(), epoch_dict["epoch_loss"],
-            phase.title(), epoch_dict["epoch_metric"]
-        )
-        bar.set_description(stats)
+                self._logger.add_histogram(f"{name}.grad", weight.grad, step)
--- metric.py
+++ metric.py
@@ -7,11 +7,11 @@ from sklearn.metrics import f1_score
 class F1Metric(nn.Module):
     def __init__(self, average="macro"):
         super(F1Metric, self).__init__()
-        self.average = average
+        self._average = average
 
     def forward(self, y_true, y_pred):
         pred_labels = np.argmax(y_pred, axis=-1)
         return f1_score(y_true=y_true,
                         y_pred=pred_labels,
-                        average=self.average,
+                        average=self._average,
                         zero_division=0.0)
